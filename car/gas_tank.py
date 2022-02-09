0897e2bb85d6106d77dfb0a087dd667013fa83f4
jgrugru <jeff.gruenbaum@gmail.com>
2022-02-09 13:16:29 -0600

    commented out pydantic condecimal since mypy error

--- car/gas_tank.py
+++ car/gas_tank.py
@@ -1,4 +1,15 @@
-from pydantic import Field, BaseModel, confloat, StrictFloat, validator, condecimal
+from email.policy import default
+from pydantic import (
+    Field,
+    BaseModel,
+    confloat,
+    StrictFloat,
+    validator,
+    condecimal,
+    ConstrainedDecimal,
+    ConstrainedFloat,
+)
+
 
 class Percent(float):
     """
@@ -17,47 +28,46 @@ class Percent(float):
     def __modify_schema__(cls, field_schema):
         # __modify_schema__ should mutate the dict it receives in place,
         # the returned value will be ignored
-        field_schema.update(
-            pattern='0.0 <= x <= 1.00',
-            type='float'
-        )
+        field_schema.update(pattern="0.0 <= x <= 1.00", type="float")
 
     @classmethod
     def validate(cls, v):
         if not isinstance(v, float):
-            raise TypeError('float required')
+            raise TypeError("float required")
         if not 0 <= v <= 1:
-            raise TypeError('Not a valid percent. Needs to be within 0.0 <= value <= 1.00')
+            raise TypeError(
+                "Not a valid percent. Needs to be within 0.0 <= value <= 1.00"
+            )
         return v
 
     def __repr__(self):
-        return f'Percent({super().__repr__()})'
+        return f"Percent({super().__repr__()})"
 
 
 class GasTank(BaseModel):
-    """The below commented out code shows how to make a percentage type 
+    """The below commented out code shows how to make a percentage type
     that is used to represent the fullness of the tank.
     There are 3 possible ways to do this in pydantic:
     - create your own class with validate and update_schema function
     - use con (constrain) pydantic types
     - create own validator function using validator decorator"""
-    
+
     capacity: int = Field(le=35, gt=0)
 
     # Very simple method to define percent. Down side it is not
     # reusable. Will have to define in multiple places.
-    # fill: confloat(ge=0, le=1) = Field(default=1)
+    # fill: condecimal(ge=0.0, le=1.0, decimal_places=2)
 
     # A variant of the above version but with more constraints:
     # Now limits on the decimal places
-    fill: condecimal(ge=0, le=1, decimal_places=2)
+    # fill: ConstrainedDecimal = Field(ge=0, le=1, decimal_places=2)
 
     # Percent is it's own class and has it's own validate function.
-    # Advantage of this is that it is reusable elsewhere as well as ability to 
+    # Advantage of this is that it is reusable elsewhere as well as ability to
     # modify the schema, more control.
-    # fill: Percent 
+    # fill: Percent
 
-    # Probably the worst choice in this case. Extra overhead and the 
+    # Probably the worst choice in this case. Extra overhead and the
     # the code is not reuseable/modular. Limits transferability of model.
     # fill: StrictFloat = Field(default=1.0)
     # @validator("fill")
@@ -70,5 +80,5 @@ class GasTank(BaseModel):
         validate_arguments = True
 
 
-x = GasTank(capacity=15, fill=0.1)
-print(x)
+# x = GasTank(capacity=15, fill=0.1)
+# print(x)
